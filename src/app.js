6eec73f6dc43629f428687039eff057ae5b6d371
atlasraw <144444822+atlasraw@users.noreply.github.com>
2023-11-29 23:51:27 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,21 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let prefixes = ["the", "our"];
+  let adjectives = ["big", "small", "red", "blue", "great", "cool"];
+  let words = ["grandma", "bear", "uppercut", "spoingus"];
+  let domains = [".com", ".cat", ".es", ".us", ".io"];
+  //Loop
+  for (let i = 0; i < prefixes.length; i++) {
+    for (let x = 0; x < adjectives.length; x++) {
+      for (let j = 0; j < words.length; j++) {
+        for (let y = 0; y < domains.length; y++) {
+          let list = document.createElement("li");
+          list.className = "text-light";
+          list.innerText = `${prefixes[i]}${adjectives[x]}${words[j]}${domains[y]}`;
+          document.querySelector("#domains").appendChild(list);
+        }
+      }
+    }
+  }
 };
